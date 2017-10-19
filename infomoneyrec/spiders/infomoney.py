196106038ec2ae865279cf7215de8c38fe9f018c
Victor Lopes <contato_victorlopes@hotmail.com>
2017-10-19 17:44:37 -0300

    Upadate information to scrapy pages

--- infomoneyrec/spiders/infomoney.py
+++ infomoneyrec/spiders/infomoney.py
@@ -3,35 +3,30 @@ from elasticsearch import Elasticsearch
 from infomoneyrec.items import InfomoneyrecItem
 from scrapy import Request
 class InfomoneyRec(Spider):
+    #define the name of the spider that will be runned 
     name = 'infomoneyrec'
     start_urls = ['http://www.infomoney.com.br/mercados/ultimas-noticias']
-    allowed_domains = ['www.infomoney.com.br']
-    #def parse(self, response):
-        #return Request("http://www.infomoney.com.br/mercados/politica/noticia/7023479/maia-diz-que-camara-vai-votar-nesta-terca-feira-urgencia",callback=self.parse_page2)
-    #def parse_page2(self, response):
-        #self.logger.info("visitado %s", response.url)
+    allowed_domains = ['www.infomoney.com.br']   
     
+    #define the method that your spider will scrapy the data from the urls declared on start_urls
     def parse(self, response):
         es = Elasticsearch([{
             'host':'localhost',
             'port':9200,
         }])
-        i = 1   
+        #count the id for the data scraped, that will be util on the elastic search
+        i = 100  
         for quote in response.css('div.column'):
             item = InfomoneyrecItem()
             item['main_url'] = response.url
             item['url'] = 'http://www.infomoney.com.br'+str(quote.css('div.section-box-secondary div.section-box-secondary-container-image a::attr(href)').extract_first())
-            request = Request(str(item['url']),callback=self.parse_page2)
+            request = Request(str(item['url']),callback=self.parse_followpage)
             request.meta['item'] = item
             yield request
-            es.index(index='testescrapy', doc_type='infomoney', id = i, body={
-                'title':item['title'],
-                'subtitle':item['subtitle'],
-                'url':item['other_url'],
-            })
+            #es.index(index='testescrapy', doc_type='infomoney', id=i, body=request)
             i = i+1
 
-    def parse_page2(self, response): 
+    def parse_followpage(self, response): 
         
         item = response.meta['item']
         item['title'] = response.css('h1.article__title::text').extract_first()
